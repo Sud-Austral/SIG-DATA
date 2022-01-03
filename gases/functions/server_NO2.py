c1eda9fb7db0304c419afb0188659e7bf9d916d0
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2022-01-03 07:58:57 -0600

    Commit

--- gases/functions/server_NO2.py
+++ gases/functions/server_NO2.py
@@ -31,18 +31,18 @@ def descargaNO2():
     # ¡¡¡IMPORTANTE!!!
     # CAMBIAR NÚMERO '1' DEL PRIMER BUCLE, SE UTILIZA SOLAMENTE EN EL PERÍODO DE PRUEBA, SUSTITUIR POR VARIABLE 'difference'
     
-
     for i in range(1):
-
-        global fechaInicial
-        global fechaFinal
-
-        global fechaI
-        global fechaF
 
         salida = []
         
         for j in filenames:
+            
+            global fechaInicial
+            global fechaFinal
+
+            global fechaI
+            global fechaF
+
             fechaInicial = startDate + timedelta(days=i)
             fechaFinal = startDate + timedelta(days=(i + 1))
 
