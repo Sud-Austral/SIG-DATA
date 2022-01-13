120f80959ae4abe37f3188833da0d7e33ee49e5a
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2022-01-13 14:24:41 -0600

    Commit

--- gases/functions/copy.py
+++ gases/functions/copy.py
@@ -1,7 +1,7 @@
 from shutil import copyfile
 
 def copiar():
-    copyfile('gases/functions/descarga/gases_CO.xlsx', 'gases/')
+    copyfile('gases/functions/descarga/gases_CO.xlsx', 'gases/gases_CO_100.xlsx')
 
 if __name__ == '__main__':
     copiar()
