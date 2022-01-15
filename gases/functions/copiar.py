f9784b5d0d876056803f090681149a2fb3ea197d
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2022-01-14 18:35:55 -0600

    Commit

--- gases/functions/copiar.py
+++ gases/functions/copiar.py
@@ -6,6 +6,7 @@ def copiar():
     copyfile('gases/functions/descarga/gases_HCHO.xlsx', 'gases/gases_HCHO.xlsx')
     copyfile('gases/functions/descarga/gases_NO2.xlsx', 'gases/gases_NO2.xlsx')
     copyfile('gases/functions/descarga/gases_SO2.xlsx', 'gases/gases_SO2.xlsx')
+    copyfile('gases/functions/descarga/gases_O3.xlsx', 'gases/gases_O3.xlsx')
 
 if __name__ == '__main__':
     copiar()
