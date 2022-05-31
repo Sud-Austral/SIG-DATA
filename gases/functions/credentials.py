0b0dd165b3e7cf9d9aba9d156cec37d9e92ad635
lmonsalve22 <lmonsalve22@gmail.com>
2022-05-31 16:38:42 -0400

    Update credentials.py

--- gases/functions/credentials.py
+++ gases/functions/credentials.py
@@ -1,6 +1,6 @@
 import os
 
-ee_token = os.environ["EARTHENGINE_TOKEN"]
+ee_token = os.environ["EARTHENGINE_TOKEN2"]
 credential = '{"refresh_token":"%s"}' % ee_token
 credential_file_path = os.path.expanduser("~/.config/earthengine/")
 os.makedirs(credential_file_path, exist_ok=True)
